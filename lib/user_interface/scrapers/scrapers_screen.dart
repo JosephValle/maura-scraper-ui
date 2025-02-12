dc4d8a374ef4d08af4c14e82137a9ba3927e3500
JosephValle <josephv2599@gmail.com>
2025-02-12 11:49:56 -0500

    refactor: remove RefreshIndicator from scrapers list for cleaner UI

--- lib/user_interface/scrapers/scrapers_screen.dart
+++ lib/user_interface/scrapers/scrapers_screen.dart
@@ -81,15 +81,12 @@ class _ScrapersScreenState extends State<ScrapersScreen> {
                     ? const Center(child: LinearProgressIndicator())
                     : const SizedBox.shrink(),
               ),
-              RefreshIndicator(
-                onRefresh: () async => _runScraper(),
-                child: Expanded(
-                  child: ListView.builder(
-                    controller: _scrollController,
-                    itemCount: state.scrapers.length,
-                    itemBuilder: (context, index) =>
-                        ScraperTile(scraper: state.scrapers[index]),
-                  ),
+              Expanded(
+                child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: state.scrapers.length,
+                  itemBuilder: (context, index) =>
+                      ScraperTile(scraper: state.scrapers[index]),
                 ),
               ),
               const Gap(8),
